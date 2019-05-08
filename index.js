2cdb5da8c4a71478c6dc212a0ba6f364d24011a5
WaynStalk <50151671+WaynStalk@users.noreply.github.com>
2019-05-08 17:18:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const client = new Discord.Client();
 client.login (process.env.TOKEN);
 
 const activities_list = [
-  "𝓦.𝓓.𝓢",
-  "𝑨 𝑫𝑰𝑺𝑪𝑶𝑹𝑫 𝑻𝑬𝑨𝑴",
-  "𝑶𝑭 𝑹𝑨𝑳𝑶𝑼𝑭𝑺",
+  "𝑤𝑆𝑐𝑎𝑚_",
+  "𝑤𝑆𝑐𝑎𝑚_",
+  "𝑤𝑆𝑐𝑎𝑚_",
     ]; // creates an arraylist containing phrases you want your bot to switch through.
 
 client.on('ready', () => {
@@ -14,7 +14,7 @@ client.on('ready', () => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
         client.user.setActivity(activities_list[index],{
             type: "STREAMING",
-            url: "https://www.twitch.tv/evil"});
+            url: "https://www.twitch.tv/wScam"});
  
     }, 3000); // Runs this every 10 seconds. 1000 = 1seconde
     console.log('Connecté');
